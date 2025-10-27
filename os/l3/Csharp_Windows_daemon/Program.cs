a7cef324e3d600d986f34e82d7739d36c54e84e6
ly0wenka <ly0wenka@gmail.com>
2025-10-27 18:51:00 +0200

    l2 cs kali

---
+++ os/l3/Csharp_Windows_daemon/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading;
+
+public class Program
+{
+    public static string ServiceName = "MyDaemonCsharp";
+    static void Main(string[] args)
+    {
+        // Створюємо новий процес фонової служби
+        RunDaemon();
+    }
+
+    static void RunDaemon()
+    {
+        // Перевіряємо чи є необхідні права для запису в журнал
+        if (!EventLog.SourceExists(ServiceName))
+        {
+            EventLog.CreateEventSource(ServiceName, "Application");
+        }
+
+        EventLog.WriteEntry(ServiceName, "Демон запущений.", EventLogEntryType.Information);
+
+        // Основний цикл роботи демона
+        while (true)
+        {
+            try
+            {
+                // Чекаємо 10 секунд перед перевіркою стану
+                Thread.Sleep(10000);
+
+                // У реальній ситуації тут можна додати код для обробки команд або подій
+                // Наприклад, можна створити обробник сигналу або події для завершення роботи
+            }
+            catch (ThreadInterruptedException)
+            {
+                EventLog.WriteEntry(ServiceName, "Демон завершив роботу через сигнал.", EventLogEntryType.Information);
+                break; // Вихід із циклу при отриманні сигналу завершення
+            }
+        }
+    }
+}
+
---
+++ os/l3/cpp_windows_daemon/Source.cpp
@@ -0,0 +1,71 @@
+#include <windows.h>
+#include <iostream>
+#include <signal.h>
+#include <fstream>
+
+SERVICE_STATUS ServiceStatus;
+SERVICE_STATUS_HANDLE hStatus;
+
+void logMessage(const std::string& message) {
+    std::ofstream logFile("C:\\Users\\L\\Downloads\\New folder(175)\\testgit\\os\\l3\\cpp_windows_daemon\\x64\\Debug\\daemon_log.txt", std::ios::app);
+    logFile << message << std::endl;
+}
+
+void signalHandler(int signal) {
+    if (signal == SIGINT) {
+        logMessage("SIGINT received. Shutting down daemon.");
+        exit(0);
+    }
+    else if (signal == SIGTERM) {  // Replacing SIGUSR1 with SIGTERM
+        logMessage("SIGTERM received. Logging signal.");
+    }
+}
+
+void ServiceMain(int argc, char** argv) {
+    ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+    ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+    ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+    hStatus = RegisterServiceCtrlHandler(L"MyDaemon", [](DWORD control) {
+        if (control == SERVICE_CONTROL_STOP) {
+            ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+            SetServiceStatus(hStatus, &ServiceStatus);
+            logMessage("Service stopped.");
+            exit(0);
+        }
+        });
+
+    SetServiceStatus(hStatus, &ServiceStatus);
+    logMessage("Service started.");
+
+    signal(SIGINT, signalHandler);
+    signal(SIGTERM, signalHandler);  // Handling SIGTERM instead of SIGUSR1
+
+    while (true) {
+        Sleep(1000);
+    }
+}
+
+void installService() {
+    SC_HANDLE schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
+    if (!schSCManager) return;
+    SC_HANDLE schService = CreateService(
+        schSCManager, L"MyDaemon", L"MyDaemon",  // Using wide-character strings
+        SERVICE_ALL_ACCESS, SERVICE_WIN32_OWN_PROCESS,
+        SERVICE_AUTO_START, SERVICE_ERROR_NORMAL,
+        L"C:\\Users\\L\\Downloads\\New folder(175)\\testgit\\os\\l3\\cpp_windows_daemon\\x64\\Debug\\cpp_windows_daemon.exe", NULL, NULL, NULL, NULL, NULL);  // Using wide-character strings
+    CloseServiceHandle(schService);
+    CloseServiceHandle(schSCManager);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1 && std::string(argv[1]) == "install") {
+        installService();
+        return 0;
+    }
+    SERVICE_TABLE_ENTRY ServiceTable[] = {
+        {LPWSTR(L"MyDaemon"), (LPSERVICE_MAIN_FUNCTION)ServiceMain},  // Using wide-character string
+        {NULL, NULL}
+    };
+    StartServiceCtrlDispatcher(ServiceTable);
+    return 0;
+}
