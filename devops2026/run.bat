a33fbb7ef8f1d4f0ee10b4b57670fb07d9f176cb
ly0wenka <ly0wenka@gmail.com>
2026-03-12 18:15:37 +0200

    devops

---
+++ devops2026/run.bat
@@ -0,0 +1,5 @@
+@echo off
+run.ps1
+choco install chafa
+curl -L https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small_2x/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg | chafa -
+pause
---
+++ devops2026/runwithoutrunps.bat
@@ -0,0 +1,4 @@
+@echo off
+choco install chafa
+curl -L https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small_2x/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg | chafa -
+pause
